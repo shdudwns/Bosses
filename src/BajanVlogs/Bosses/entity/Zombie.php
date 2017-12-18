5cf9109d8fca17378ab1d92a4ce8f46c56f571ef
BajanVlogs <kjolly544@gmail.com>
2017-12-18 15:53:46 -0500

    Create Zombie.php

---
+++ src/BajanVlogs/Bosses/entity/Zombie.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace BajanVlogs\Bosses\entity;
+
+use pocketmine\entity\Entity;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\item\Item;
+
+class Zombie extends Monster{
+    const NETWORK_ID = 32;
+
+    public $width = 0.72;
+    public $height = 1.8;
+
+    protected $speed = 1.1;
+
+    public function initEntity(){
+        if(isset($this->namedtag->Health)){
+            $this->setHealth((int) $this->namedtag["Health"]);
+        }else{
+            $this->setHealth($this->getMaxHealth());
+        }
+        $this->setMinDamage([0, 3, 4, 6]);
+        $this->setMaxDamage([0, 3, 4, 6]);
+        parent::initEntity();
+        $this->created = true;
+    }
+
+    public function getName() : string {
+        return "Zombie";
+    }
+
+    public function attackEntity(Entity $player){
+        if($this->attackDelay > 10 && $this->distanceSquared($player) < 2){
+            $this->attackDelay = 0;
+            $ev = new EntityDamageByEntityEvent($this, $player, EntityDamageEvent::CAUSE_ENTITY_ATTACK, $this->getDamage());
+            $player->attack($ev);
+        }
+    }
+
+    public function getDrops() : array {
+        $drops = [];
+        if($this->lastDamageCause instanceof EntityDamageByEntityEvent){
+            switch(mt_rand(0, 2)){
+                case 0:
+                    $drops[] = Item::get(Item::FEATHER, 0, 1);
+                    break;
+                case 1:
+                    $drops[] = Item::get(Item::CARROT, 0, 1);
+                    break;
+                case 2:
+                    $drops[] = Item::get(Item::POTATO, 0, 1);
+                    break;
+            }
+        }
+        return $drops;
+    }
+
+}
