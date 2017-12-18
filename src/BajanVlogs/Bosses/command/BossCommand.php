43238e63da43b5f8e0181b1c2632530bd8ee8fbc
BajanVlogs <kjolly544@gmail.com>
2017-12-18 16:34:58 -0500

    Update BossCommand.php

--- src/BajanVlogs/Bosses/command/BossCommand.php
+++ src/BajanVlogs/Bosses/command/BossCommand.php
@@ -1,21 +1,34 @@
 <?php
 
-declare(strict_types=1);
-
 namespace BajanVlogs\Bosses\command;
 
-use pocketmine\command\CommandSender;
-use pocketmine\command\defaults\VanillaCommand;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Command\Command;
+use pocketmine\Command\CommandSender;
+use pocketmine\entity\Effect;
 
-class BossCommand extends VanillaCommand {
-	public function __construct($name){
-		parent::__construct($name,
-			"A Description",
-			"/plugin",
-			[]);
+class BossCommand extends PluginBase{
+	
+	public function onEnable(){
+		$this->getServer()->getLogger()->info("Bosses enabled!");
+	}
+	
+	public function onDisable(){
+		$this->getServer()->getLogger()->info("Bosses disabled!");
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+		
+		switch($cmd->getName()){
+			
+			case "boss":
+				if($sender instanceof Player){
+					$sender->addTitle("§c§lBosses§b§l", "§a§lBy BajanVlogs", 20, 40, 20);
+				}
+			break;
+		}
+		return true;
 	}
 
-	public function execute(CommandSender $sender, string $commandLabel, array $args){
-		// TODO: Do stuff like more bosses.
-	}
 }
